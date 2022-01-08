4f1b59b60c0e24de3a5d723c494bc4932c87c7b2
Bochner <randy.bochner@gmail.com>
2022-01-08 16:46:50 -0500

    Create rtoj_sugg.c

---
+++ rtoj_sugg.c
@@ -0,0 +1,121 @@
+
+// Misc. Suggestions
+- Make the "hail" command usable by all characters at level one, transports all followers from anywhere in the game to your current location. Completely remove Call Droid from the game. This is a QoL thing that isnt breaking anything. It is utterly annoying to constantly transport followers or have to micro them.
+
+- Make unkillable trainers for all professions. Legends running around killing everything and not being able to level or train is extremely annoying, especially for newbies. (Especially now that all GMs are legends)
+
+- Make skills learned when gaining a level actually raise from using it in case you have no practices. (If you can already, the chance is astronomically small. I used Overcharge Pistol literally hundreds of times, never succeeded once, never got a skill up from awful.)
+
+- Remove subbing gear innately to every character. I have heard multiple times that this game is referred to as diablo MUD with star wars. Most players do not want to lose things, there is a reason why "soulslike and roguelike" games have such a small fan base. Instead make it like hardcore mode in diablo, you can elect to receive some kind of bonus like serving the emporer and the negative is random chance to subb gear. Or just remove it all together! Mechanics like subbing gear dont make anything more challenging really, just pisses people off.
+
+- Remove Hunger / Thirst from the game as a detriment. It spams you out unncessisarily and it very annoying. Instead of negative, turn it into an optional positive. Not being hungry or thirsty makes you "well fed" and you regen faster or take less damage or something.
+
+- Grouping incentives. Increased exp while grouping, increased drop rates, bonus to stats. Just something that gives an incentive to group. While the sole focus of the game is not to group, there should be SOME incentive for doing it. 
+
+- Gear at appropriate levels
+
+
+
+// Medic Suggestions
+Skill                    Level Learned
+-------                 ---------------
+Pack Bag                      1
+Create Droid                  1
+    // EV-A4 Medical Droid, does minor melee damage, constantly puts a heal over time on the medic and poisons the enemy, hp/stats/healing/damage scales with level up to 100
+    // ONLY useable if Medic is your primary profession
+Droids available to the all Primary Class Medics:
+ 
+Level        Droid                       Keyword       Materials
+--------    ---------------             ---------     -------------------
+Level 1     EV-A4 Medical Droid          ev           D(5), N(1)
+    // maybe materials scale with level since healing/damage does too
+//
+Treat Injury                  1
+    // Rework this skill similar to how create droid and manufacture are, where its one command that unlocks multiple you level 
+    Minor                     1
+    Moderate                  13
+    Group                     22
+    Serious                   38
+    Greater Group             52
+Defibrillator                 5
+    // increase damage and make it stun all mobs instead of just droids, pretty sure a defib or taser stuns people in real life all the time
+Refill Syringe                8
+Diagnose                     10
+    // rework to show additional data, like all buffs/debuffs on a target
+Manufacture Pills            18
+Shield Slam                  24
+Medical Monitoring           26
+Defensive Fighting           28
+Biological Warfare           30
+
+Cure Blindness               34
+Group Medical Monitoring     34
+Fast Hands                   36
+Non-Lethal Weapons           42
+Throw                        45
+Offensive Fighting           48
+Anatomy Expert               50
+    // Trained medics are experts in the anatomy and physiology of living beings/creatures. 
+    // 10% increased chance to crit on all living beings/creatures and crits have a chance to make the target bleed due to knowing where to strike vital points of anatomy.
+Skilled Hands                56
+Heal Minor Wound             59
+
+// removed / reworked
+//Treat Moderate Injury        13 // remove from help file, add to new under "Treat Injury"
+//Treat Group Injury           22 // remove from help file, add to new under "Treat Injury"
+//Treat Serious Injury         38 // remove from help file, add to new under "Treat Injury"
+//Treat Greater Group Injury   52 // remove from help file, add to new under "Treat Injury"
+
+// Combat Medic
+Skill                    Level Learned
+-----                    -------------
+Combat Healing                60
+Combat Diagnostics            65
+    // Combat Medics are trained to scan enemies for their weaknesses
+    // Augments Diagnose, shows additional flags like !trip on target and puts a "Target Diagnosed" defbuff on target that increases the rate it takes criticals due to combat medic telling the party its diagnosed weaknesses
+    // Usable while combat healing
+Professional First Aid        70
+Take Cover                    75
+Medic Neutrality              80
+Under Fire                    85
+Medical Evacuation            90
+    // Skill seems pretty dumb in the current state of the game, complete rework to a passive
+    // Combat Medic has the ability to call in a medevac for any party member who reaches critical condition.
+    // Prevents death from a mob or DT (including sub) and transports them to the mos eisley surgeon
+    // Does not prevent wounds
+Boost Morale                 100
+
+// Casualty Evacuation           65 // remove, pretty useless ability
+
+// Surgeon
+Skill                    Level Learned
+-------                  -------------
+Dissect                      60
+Transfusion                  66
+Surgical Removal             72
+Heal Serious Wound           78
+Reconstruct Body             84
+Surgical Strike              90
+Heal Critical Wound          95
+Euthanize                   100
+
+
+// Bounty Hunter Suggestions
+
+// Technician Suggestions
+
+// Droid Handler Suggestions
+Skill                    Level Learned
+-------                 ---------------
+Labor Droid                   1
+Analyze Droid                 4
+Repair Lesser                 6
+Call Droid                    8
+Droid Trick                  12
+Tune Up                      16
+Self Destruct                18
+Crab Droid                   20
+
+// Officer Suggestions
+
+
